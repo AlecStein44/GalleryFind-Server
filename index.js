6a187d26a7a437daf5e2ffb9563b193c9b7b3f59
AlecStein44 <45604055+AlecStein44@users.noreply.github.com>
2020-01-25 22:49:43 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const knex = require('knex')
 const cors = require('cors');
 const app = express();
 app.use(cors());
-//app.use(express.static('public'));
+app.use(express.static('public'));
 app.use(express.json());
 
 const db = knex({
