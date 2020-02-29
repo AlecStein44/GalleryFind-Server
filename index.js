0d95a46ae05de5596318052008abef7c12c8f7f7
AlecStein44 <45604055+AlecStein44@users.noreply.github.com>
2020-02-29 13:30:17 -0800

    Update index.js

--- index.js
+++ index.js
@@ -102,15 +102,17 @@ app.get('/login', (req, res) => {
     .where({username: req.query.username},{password: req.query.password})
     .then(data => {
       if(data.password === req.query.password) {   
-        .then(data => {
+        return .then(data => {
           return (
             res.json(data),
             console.log(data)
           )
         })
       } else {
-        res.status(401),
-        console.log('error happened')
+        return (
+          res.status(401),
+          console.log('error happened')
+        )
       }
     console.log(req.query.search)
 })
