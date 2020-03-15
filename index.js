d9a21f49d785c563e0757eb5a6032ecd9f61cd04
AlecStein44 <45604055+AlecStein44@users.noreply.github.com>
2020-03-15 14:52:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -69,8 +69,8 @@ app.post('/signup', (req, res) => {
         db
             .select('*')
             .from('userdata')
-            .where("email", email)
-            .andWhere("username", username)
+            .where("email": fields.email)
+            .andWhere("username": fields.username)
             .then(emailuserList => {
                 if (emailuserList.length === 0) {
                     return db('userdata')
